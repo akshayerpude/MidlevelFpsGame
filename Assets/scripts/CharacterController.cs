7b788e872d8ed16cd208675967325291beef8dfa
Akshay456 <akshayerpude@gmail.com>
2022-01-25 11:43:21 +0530

    Camera added to the player,  forward movement fixed.

--- Assets/scripts/CharacterController.cs
+++ Assets/scripts/CharacterController.cs
@@ -9,6 +9,13 @@ public class CharacterController : MonoBehaviour
     public float PlayerJumpValue;
     private bool isGrounded;
     private CapsuleCollider capsulecollider;
+    public GameObject cam;
+    private Quaternion camRotation;
+    private Quaternion playerRotation;
+    public float mouseSens;
+    public float minX = -90.0f;
+    public float maxX = 90.0f;
+
 
     private void Awake()
     {
@@ -18,7 +25,8 @@ public class CharacterController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        camRotation = cam.transform.localRotation;
+        playerRotation = transform.localRotation;
     }
 
     // Update is called once per frame
@@ -26,7 +34,25 @@ public class CharacterController : MonoBehaviour
     {
         PlayerMovement();
         PlayerJumpMovement();
+        float mousex = Input.GetAxis("Mouse X") * mouseSens;
+        float mousey = Input.GetAxis("Mouse Y") * mouseSens;
+        camRotation = camRotation * Quaternion.Euler(-mousey, 0, 0);
+        playerRotation = playerRotation * Quaternion.Euler(0,mousex,0);
+        transform.localRotation = playerRotation;
+        cam.transform.localRotation = camRotation;
+        camRotation = ClampRotationOnXaxis(camRotation);
+    }
 
+    Quaternion ClampRotationOnXaxis(Quaternion value)
+    {
+        value.x /= value.w;
+        value.y /= value.w;
+        value.z /= value.w;
+        value.w = 1.0f;
+        float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(value.x);
+        angleX = Mathf.Clamp(angleX, minX, maxX);
+        value.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
+        return value;
     }
 
     bool PlayerGrounded()
@@ -46,8 +72,9 @@ public class CharacterController : MonoBehaviour
     {
         float horizontalMovement = Input.GetAxis("Horizontal") * playerSpeed;
         float forwardMovement = Input.GetAxis("Vertical") * playerSpeed;
-        transform.position += new Vector3(horizontalMovement,0,forwardMovement);
-    }
+        //transform.position += new Vector3(horizontalMovement,0,forwardMovement);
+          transform.position+=cam.transform.forward * forwardMovement+cam.transform.right*horizontalMovement;
+  }
 
     void PlayerJumpMovement()
     {
